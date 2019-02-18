56ed2ad281fad1bcb44adddf3a0755f04790c6c6
MrTj458 <mrtj458@gmail.com>
2019-02-17 20:46:09 -0700

    Validation and more async/await

--- routes/api/users.js
+++ routes/api/users.js
@@ -7,6 +7,10 @@ const keys = require('../../config/keys')
 const User = require('../../models/User')
 const passport = require('passport')
 
+// Input validation
+const validateRegisterInput = require('../../validation/register')
+const validateLoginInput = require('../../validation/login')
+
 // @route GET api/users/test
 // @desc Tests users route
 // @access Public
@@ -16,53 +20,66 @@ router.get('/test', (req, res) => res.json({ msg: 'Users Works' }))
 // @desc Register User
 // @access Public
 router.post('/register', async (req, res) => {
+	// Check validation
+	const { errors, isValid } = validateRegisterInput(req.body)
+	if (!isValid) {
+		return res.status(400).json(errors)
+	}
+
 	// Check is user already exists
 	if (await User.findOne({ email: req.body.email })) {
-		return res.status(400).json({ email: 'User already exists' })
+		errors.email = 'Email already exists'
+		return res.status(400).json(errors)
 	}
 
 	// Find gravatar
 	const avatar = gravatar.url(req.body.email, {
-		s: '200',
-		r: 'pg',
-		d: 'mm',
+		s: '200', // Size
+		r: 'pg', // Rating
+		d: 'mm', // Default
 	})
 
 	// Create new user
-	const user = new User({
+	const newUser = new User({
 		name: req.body.name,
 		email: req.body.email,
 		password: req.body.password,
 		avatar: avatar,
 	})
 
-	// Encrypt user password and send back
-	bcrypt.genSalt(10, async (err, hash) => {
-		if (err) throw err
+	// Hash Password
+	const salt = await bcrypt.genSalt(10)
+	const hash = await bcrypt.hash(newUser.password, salt)
+	newUser.password = hash
 
-		user.password = hash
-		const newUser = await user.save()
-
-		res.json(newUser)
-	})
+	// Save user and send it back
+	res.json(await newUser.save())
 })
 
 // @route POST /api/users/login
 // @desc Login user / Returning JWT
 // @access public
 router.post('/login', async (req, res) => {
+	// Check validation
+	const { errors, isValid } = validateLoginInput(req.body)
+	if (!isValid) {
+		res.status(400).json(errors)
+	}
+
 	const email = req.body.email
 	const password = req.body.password
 
 	const user = await User.findOne({ email })
 	// Check is user exists
 	if (!user) {
-		return res.json({ email: 'User not found' })
+		errors.email = 'User not found'
+		return res.json(errors)
 	}
 
 	// Check if password is correct
 	if (!(await bcrypt.compare(password, user.password))) {
-		return res.status(400).json({ password: 'Password incorrect' })
+		errors.password = 'Password Incorrect'
+		return res.status(400).json(errors)
 	}
 
 	// Set JWT payload
@@ -72,12 +89,13 @@ router.post('/login', async (req, res) => {
 		avatar: user.avatar,
 	}
 
-	// Sign JWT and send it
-	jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 }, (err, token) => {
-		res.json({
-			success: true,
-			token: 'Bearer ' + token,
-		})
+	// Sign JWT
+	const token = await jwt.sign(payload, keys.secretOrKey, { expiresIn: 3600 })
+
+	// Send back JWT
+	res.json({
+		success: true,
+		token: 'Bearer ' + token,
 	})
 })
 
