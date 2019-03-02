16e47638677e0797040f7cbee19fb0d9433a7f83
MrTj458 <mrtj458@gmail.com>
2019-03-01 19:32:39 -0700

    Get and delete posts

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -4,6 +4,7 @@ const mongoose = require('mongoose')
 const passport = require('passport')
 
 const Post = require('../../models/Post')
+const Profile = require('../../models/Profile')
 
 const validatePostInput = require('../../validation/post')
 
@@ -11,6 +12,28 @@ const validatePostInput = require('../../validation/post')
 // @desc Tests posts route
 // @access Public
 router.get('/test', (req, res) => res.json({ msg: 'Posts Works' }))
+
+// @route GET api/posts
+// @desc Get posts
+// @access Public
+router.get('/', (req, res) => {
+	Post.find()
+		.sort({ date: -1 })
+		.then((posts) => res.json(posts))
+		.catch((err) => res.status(404).json({ nopostsfound: 'No posts found' }))
+})
+
+// @route GET api/posts/:id
+// @desc Get post by id
+// @access Public
+router.get('/:id', (req, res) => {
+	Post.findById(req.params.id)
+		.sort({ date: -1 })
+		.then((post) => res.json(post))
+		.catch((err) =>
+			res.status(404).json({ nopostfound: 'No post found with that ID' })
+		)
+})
 
 // @route POST api/posts
 // @desc Create a post
@@ -22,7 +45,7 @@ router.post(
 		const { errors, isValid } = validatePostInput(req.body)
 
 		if (!isValid) {
-			res.status(400).json(errors)
+			return res.status(400).json(errors)
 		}
 
 		const newPost = new Post({
@@ -32,7 +55,33 @@ router.post(
 			user: req.user.id,
 		})
 
-		newPost.save().then((post) => res.json(post))
+		newPost
+			.save()
+			.then((post) => res.json(post))
+			.catch((err) => console.log(err))
+	}
+)
+
+// @route DELETE api/posts/:id
+// @desc Delete post
+// @access Private
+router.delete(
+	'/:id',
+	passport.authenticate('jwt', { session: false }),
+	(req, res) => {
+		Profile.findOne({ user: req.user.id }).then((profile) => {
+			Post.findById(req.params.id)
+				.then((post) => {
+					if (post.user.toString() !== req.user.id) {
+						return res
+							.status(401)
+							.json({ notauthorized: 'User not authorized' })
+					}
+
+					post.remove().then(() => res.json({ success: true }))
+				})
+				.catch((err) => res.status(404).json({ postnotfound: 'No post found' }))
+		})
 	}
 )
 
